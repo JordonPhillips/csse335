1617b7174e4738e74e4dadc0fd8be78b546a098a
Jordon Phillips <phillijk@rose-hulman.edu>
2014-03-16 18:47:45 -0400

    finished hw2.2

--- hw2/guessMyNumber.c
+++ hw2/guessMyNumber.c
@@ -1,3 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+
 int main(int argc, char* argv[]) {
+    long long n = 0;
+    long long inc = 1;
+    int direction = 1;
+    char ch = '\n';
+
+    printf("Think of a number, and I'll try to guess it!\n");
+    while (1) {
+        do {
+            if (ch == '\n') printf("Is your number %lld\? ('y' for yes, 'n' for no)\n", n);
+            ch = getchar();
+            while (ch ==32) ch = getchar();
+        } while (ch != 'y' && ch != 'n');
+
+        if (ch == 'y') break;
+
+        do {
+            if (ch == '\n') printf("Was my guess too high ('h') or too low ('l')?\n");
+            ch = getchar();
+            while (ch == 32) ch = getchar();
+        } while (ch != 'h' && ch != 'l');
+
+        if (ch == 'l') {
+            if (direction == -1) inc = 1;
+            direction = 1;
+
+            n += inc;
+            inc += inc;
+        } else if (ch == 'h') {
+            if (direction == 1) inc = 1;
+            direction = -1;
+
+            n -= inc;
+            inc += inc;
+        }
+    }
+
+    printf("Your number is %lld!\n", n);
     return 0;
 }
--- hw2/old.c
+++ hw2/old.c
@@ -2,12 +2,25 @@
 #include <stdio.h>
 
 #define MY_BIRTH_YEAR 1993
-#define BUFFSIZE 80
+#define BUFFSIZE 256
 
 int main(int argc, char* argv[]) {
-  printf("What's your name?\n");
-  getchar();
+  char buffer[BUFFSIZE];
+  char *end = NULL;
+  int year;
+  int isInt;
 
-  printf("What year were you born in?\n");
+  printf("What's your name?\n");
+  fgets(buffer, BUFFSIZE, stdin);
+
+  while (1) {
+    printf("What year were you born in?\n");
+    fflush(stdout);
+    fgets(buffer, BUFFSIZE, stdin);
+    isInt = sscanf(buffer, "%d", &year);
+    if (isInt) break;
+  }
+
+  if (year < MY_BIRTH_YEAR) printf("Old.\n");
   return 0;
 }
