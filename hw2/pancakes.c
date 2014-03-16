cd73e1b72d0c866e3bafb8458b9d1e6b552f17c9
Jordon Phillips <phillijk@rose-hulman.edu>
2014-03-16 18:56:39 -0400

    finished hw2.3

--- hw2/pancakes.c
+++ hw2/pancakes.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+
+#define NUM_DINERS 10
+
 int main(int argc, char* argv[]) {
+    int diners[NUM_DINERS];
+    int i, j, n;
+
+    for (i = 0; i < NUM_DINERS; i++) {
+        printf("How many pancakes did person %d eat? ", i+1);
+        scanf("%d", &n);
+        diners[i] = n;
+    }
+
+    n = diners[0];
+    for (i = 1; i < NUM_DINERS; i++) {
+        if (n < diners[i]) {
+            n = diners[i];
+            j = i;
+        }
+    }
+
+    printf("Person %d ate the most pancakes.\n", j+1);
     return 0;
 }
