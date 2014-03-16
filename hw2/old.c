1622c7845e0a2a57610ecc593db0938d5cb2f901
Jordon Phillips <phillijk@rose-hulman.edu>
2014-03-16 16:56:03 -0400

    Initial Commit

---
+++ hw2/old.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MY_BIRTH_YEAR 1993
+#define BUFFSIZE 80
+
+int main(int argc, char* argv[]) {
+  printf("What's your name?\n");
+  getchar();
+
+  printf("What year were you born in?\n");
+  return 0;
+}
