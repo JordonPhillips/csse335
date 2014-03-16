c8fef1abf9712db61240fccccab6c141e2dfad8e
Jordon Phillips <phillijk@rose-hulman.edu>
2014-03-16 19:42:02 -0400

    finished hw2.5

--- hw2/pancakes.c
+++ hw2/pancakes.c
@@ -18,17 +18,20 @@ int main(int argc, char* argv[]) {
     for (i = 0; i < NUM_DINERS; i++) {
         printf("How many pancakes did person %d eat? ", i+1);
         scanf("%d", &n);
-
-        d = (diner*)malloc(sizeof(diner));
-        *d = (diner){i, n};
-        diners[i] = *d;
-        free(d);
+        diners[i] = (diner){i,n};
     }
 
     qsort(diners, NUM_DINERS, sizeof(diner), dinercmp);
 
     printf("Person %d ate the most pancakes.\n", diners[NUM_DINERS-1].index + 1);
     printf("Person %d ate the least pancakes.\n", diners[0].index + 1);
+    printf("The following is an ordering (from least to greatest) of number of pancakes eaten:\n");
+
+    printf("%d", diners[0].index + 1);
+    for (i = 1; i < NUM_DINERS; i++) {
+        printf(", %d", diners[i].index + 1);
+    }
+    printf("\n");
     return 0;
 }
 
