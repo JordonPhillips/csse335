78b450668b15c22dba099f25e19693a93eea2210
Jordon Phillips <phillijk@rose-hulman.edu>
2014-03-16 21:17:45 -0400

    finished hw2.8

--- hw2/printN.c
+++ hw2/printN.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+
+#define BUFFSIZE 1024
+
 int main(int argc, char* argv[]) {
+    char buffer[BUFFSIZE];
+    int n, isInt, i, j;
+
+    while (1) {
+        printf("Please enter an integer: ");
+        fgets(buffer, BUFFSIZE, stdin);
+        isInt = sscanf(buffer, "%d", &n);
+        if (isInt) break;
+    }
+
+    for (i = n; i > 0; i--) {
+        for (j = 0; j < n - i; j++)
+            printf(" ");
+        for (j = 0; j < i; j++)
+            printf("*");
+        printf("\n");
+    }
+
     return 0;
 }
