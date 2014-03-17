baaec6a9c2ce39d4bc48e5def791f73906355667
Jordon Phillips <phillijk@rose-hulman.edu>
2014-03-16 20:57:13 -0400

    finished hw2.6

--- hw2/sum.c
+++ hw2/sum.c
@@ -1,3 +1,23 @@
+#include <stdio.h>
+
+#define N 10
+#define BUFFSIZE 1024
+
 int main(int argc, char* argv[]) {
+    int i, isInt, n, nsum = 0;
+    char buffer[BUFFSIZE];
+
+    for (i = 0; i < N; i++) {
+        while (1) {
+            printf("Please enter an integer (%d more): ", N - i);
+            fflush(stdout);
+            fgets(buffer, BUFFSIZE, stdin);
+            isInt = sscanf(buffer, "%d", &n);
+            if (isInt) break;
+        }
+        nsum += n;
+    }
+
+    printf("The sum of those numbers is %d.\n", nsum);
     return 0;
 }
