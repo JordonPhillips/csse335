3a6dac27115e1f136c4e01a3d456d6d597f215ac
Jordon Phillips <phillijk@rose-hulman.edu>
2014-03-18 21:03:48 -0400

    Finished hw3.1 and hw3.2

--- hw3/p1.c
+++ hw3/p1.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_ELEMENTS 10
+
+int countEven(int* arr, int size);
 
 int main(int argc, char *argv[]) {
+    int *arr = (int*)malloc(NUM_ELEMENTS*sizeof(int));
+    int i;
+
+    for (i = 0; i < NUM_ELEMENTS; i++)
+        arr[i] = i+1;
+
+    printf("There are %d even numbers from 1 to %d\n",
+            countEven(arr, NUM_ELEMENTS), NUM_ELEMENTS);
+
+    free(arr);
     return 0;
+}
+
+int countEven(int* arr, int size) {
+    int* ptr = arr;
+    int i, count = 0;
+
+    for (i = 0; i < size; i++) {
+        if ((*ptr % 2) == 0) count++;
+        ptr++;
+    }
+
+    return count;
 }
--- hw3/p2.c
+++ hw3/p2.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 15
+
+double* maximum(double* a, int size);
 
 int main(int argc, char *argv[]) {
+    double* arr = (double*)malloc(SIZE*sizeof(double));
+    int i;
+
+    for (i = 0; i < SIZE; i++)
+        arr[i] = i + 1;
+
+    printf("The max of a double array that goes from 1 to %d is: %f\n",
+            SIZE, *(maximum(arr, SIZE)));
+
+    free(arr);
     return 0;
+}
+
+double* maximum(double* a, int size) {
+    double *p1 = a;
+    int i;
+    for (i = 1; i < size; i++)
+        if (*p1 < *(a+i)) p1++;
+    return p1;
 }
