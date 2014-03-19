3e97d13c5d50dfa5cba11f2d97a793010cae7337
Jordon Phillips <phillijk@rose-hulman.edu>
2014-03-18 21:57:58 -0400

    Finished hw3.5

--- hw3/p4.c
+++ hw3/p4.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+int myStrLen(char* s);
+
 int main(int argc, char *argv[]) {
+    char *str = "MALO MORI QUAM FOEDARI";
+    printf("The length of the string '%s' is: %d\n", str, myStrLen(str));
     return 0;
+}
+
+int myStrLen(char* s) {
+    int len;
+    char *p = s;
+    for (len = 0; p[len] != '\0'; len++) ;
+    return len;
 }
--- hw3/p5-6.c
+++ hw3/p5-6.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 256
+
+void revString(char* s);
 
 int main(int argc, char *argv[]) {
+    char *str = (char*)malloc(SIZE*sizeof(char));
+    strcpy(str,"MALO MORI QUAM FOEDARI");
+    printf("The reverse of '%s' ", str);
+    revString(str);
+    printf("is '%s'\n", str);
+    free(str);
     return 0;
+}
+
+void revString(char* a) {
+    char *p1 = a, *p2 = a, tmp;
+    while (*p1 != '\0') p1++;
+    while (p1 != p2) {
+        p1--;
+        tmp = *p1;
+        *p1 = *p2; // SEG FAULT
+        *p2 = tmp;
+        p2++;
+    }
 }
