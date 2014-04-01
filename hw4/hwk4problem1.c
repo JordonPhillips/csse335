799798177738bf009a5e0f09675906e270020132
Jordon Phillips <phillijk@rose-hulman.edu>
2014-03-31 23:09:13 -0400

    added in the mallocs

--- hw4/hwk4problem1.c
+++ hw4/hwk4problem1.c
@@ -1,5 +1,6 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
@@ -11,6 +12,7 @@ int main(int argc, char** argv) {
     int i;
     int* n;
     while(1) {
+      n = (int*)malloc(sizeof(int));
       printf("Input: ");
       scanf("%d", n);
       if (*n < 0) break;
@@ -18,12 +20,14 @@ int main(int argc, char** argv) {
       for (i = 1; i < total_procs; i++) {
         MPI_Send(n,1,MPI_INT,i,0,MPI_COMM_WORLD);
       }
+      free(n);
     }
   } else {
     MPI_Status status;
-    int* recv;
+    int* recv = (int*)malloc(sizeof(int));
     MPI_Recv(recv,1,MPI_INT,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
     printf("Rank: %d Received: %d",rank,*recv);
+    free(recv);
   }
 
   MPI_Finalize();
