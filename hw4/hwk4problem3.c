a6adfd0edbfdd12da570ab5da6c05cfd7aa0d1e7
Jordon Phillips <phillijk@rose-hulman.edu>
2014-04-01 01:17:09 -0400

    Stab 2 at hw4.3

--- hw4/hwk4problem3.c
+++ hw4/hwk4problem3.c
@@ -1,6 +1,7 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 void source(int rank, int dest_rank, char** argv);
 void destination(int rank, int source_rank, char** argv);
@@ -35,7 +36,8 @@ void source(int rank, int dest_rank, char** argv) {
     char bytes[size];
 
     MPI_Status status;
-    double starttime, endtime, total;
+    double starttime, endtime;
+    double total = 0;
     int i;
     for (i = 0; i < samples; i++) {
         starttime = MPI_Wtime();
@@ -44,14 +46,24 @@ void source(int rank, int dest_rank, char** argv) {
         MPI_Recv(bytes,size,MPI_CHAR,dest_rank,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
 
         endtime = MPI_Wtime();
-        total += size/(2*(endtime-starttime));
+        total += endtime-starttime;
     }
-    total = total/samples;
+    total /= samples;
+    total *= MPI_Wtick();
+
+    total = size/(2*total);
 
     printf("Data Size: %dB\n",size);
     printf("Number of Samples: %d\n",samples);
     printf("Connection Speed between rank %d and %d was %f MB/s\n", rank, dest_rank, total);
     printf("\nHere is the machine-process pairing\n\n");
+
+    char name[1000];
+    gethostname(name,1000);
+    printf("Rank %d: %s\n", rank, name);
+
+    int n = 0;
+    MPI_Send(&n,1,MPI_INT,dest_rank,0,MPI_COMM_WORLD);
 }
 
 void destination(int rank, int source_rank, char** argv) {
@@ -65,4 +77,11 @@ void destination(int rank, int source_rank, char** argv) {
         MPI_Recv(bytes,size,MPI_CHAR,source_rank,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
         MPI_Send(bytes,size,MPI_CHAR,source_rank,0,MPI_COMM_WORLD);
     }
+
+    int n;
+    MPI_Recv(&n,1,MPI_INT,source_rank,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
+
+    char name[1000];
+    gethostname(name,1000);
+    printf("Rank %d: %s\n", rank, name);
 }
