b8a6f61dbde57223b794d16199c5aaab07e16531
Jordon Phillips <phillijk@rose-hulman.edu>
2014-04-01 00:04:34 -0400

    Hopefully completed hw4.2. Also updated the skeletons for the other problems

--- hw4/hwk4problem2.c
+++ hw4/hwk4problem2.c
@@ -1,9 +1,45 @@
 #include <mpi.h>
 #include <stdio.h>
 
+void master(int total_procs);
+void slave(int rank, int total_procs);
+
 int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
+  int total_procs, rank;
+  MPI_Comm_size(MPI_COMM_WORLD,&total_procs);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+  if (rank == 0) {
+    master(total_procs);
+  } else {
+    slave(rank, total_procs);
+  }
 
   MPI_Finalize();
   return 0;
+}
+
+void master(int total_procs) {
+    int n;
+    printf("Input: ");
+    fflush(stdout);
+    scanf("%d",&n);
+
+    if (total_procs > 1) {
+        MPI_Send(&n,1,MPI_INT,1,0,MPI_COMM_WORLD);
+    }
+}
+
+void slave(int rank, int total_procs) {
+    int n;
+    MPI_Status status;
+    MPI_Recv(&n,1,MPI_INT,rank-1,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
+
+    printf("Rank: %d Sender: %d Data %d", rank, rank-1, n);
+    fflush(stdout);
+
+    if (total_procs - rank > 1) {
+        MPI_Send(&n,1,MPI_INT,rank+1,0,MPI_COMM_WORLD);
+    }
 }
--- hw4/hwk4problem4.c
+++ hw4/hwk4problem4.c
@@ -1,9 +1,27 @@
 #include <mpi.h>
 #include <stdio.h>
 
+void master(int total_procs);
+void slave(int rank, int total_procs);
+
 int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
+  int total_procs, rank;
+  MPI_Comm_size(MPI_COMM_WORLD,&total_procs);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+  if (rank == 0) {
+    master(total_procs);
+  } else {
+    slave(rank, total_procs);
+  }
 
   MPI_Finalize();
   return 0;
+}
+
+void master(int total_procs) {
+}
+
+void slave(int rank, int total_procs) {
 }
