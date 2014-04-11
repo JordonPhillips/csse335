07f78d0f0150974ba65141524f233d20dd62332d
Jordon Phillips <phillijk@rose-hulman.edu>
2014-04-10 21:16:02 -0400

    First shot at gather

--- hw5/MY_Gather.c
+++ hw5/MY_Gather.c
@@ -1,8 +1,31 @@
 #include <mpi.h>
 
+#define MY_GATHER_TAG 45
+
 void MY_Gather(void *sendbuf, int sendcount, MPI_Datatype sendtype,
     void *recvbuf, int recvcount, MPI_Datatype recvtype, int root,
     MPI_Comm comm) {
+    int rank;
+    MPI_Comm_rank(comm, &rank);
 
+    if (rank == root) {
+        MPI_Status status;
+        int total_procs, data_size;
+        MPI_Comm_size(comm, &total_procs);
+        MPI_Type_size(recvtype, &data_size);
+
+        data_size *= recvcount;
+        void *data = recvbuf;
+
+        int i;
+        for (i = 0; i < total_procs; i++) {
+            if (i != root) {
+                MPI_Recv(data,recvcount,recvtype,i,MY_GATHER_TAG,comm,&status);
+                data = (char*)data + data_size;
+            }
+        }
+    } else {
+        MPI_Send(sendbuf,sendcount,sendtype,root,MY_GATHER_TAG,comm);
+    }
 }
 
--- hw5/MY_Gather_driver.c
+++ hw5/MY_Gather_driver.c
@@ -1,6 +1,9 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "MY_MPI.h"
+
+#define NUM_INTS 4
 
 void master(int total_procs);
 void slave(int rank, int total_procs);
@@ -22,7 +25,28 @@ int main(int argc, char** argv) {
 }
 
 void master(int total_procs) {
+    int total_ints = NUM_INTS*(total_procs-1);
+    int *nums = (int*)malloc(total_ints*sizeof(int));
+
+    MPI_Gather(NULL, 0, NULL, nums, NUM_INTS, MPI_INT, 0, MPI_COMM_WORLD);
+
+    printf("Master recieved: [\n");
+    int i;
+    for (i = 0; i < total_ints; i++) {
+        printf("\t%d,\n",nums[i]);
+    }
+    printf("]");
+    fflush(stdout);
+
+    free(nums);
 }
 
 void slave(int rank, int total_procs) {
+    int *nums = (int*)malloc(NUM_INTS*sizeof(int));
+    int i;
+    for (i = 0; i < NUM_INTS; i++) {
+        nums[i] = (NUM_INTS*(rank-1))+i+1;
+    }
+    MPI_Gather(nums,NUM_INTS,MPI_INT,NULL,0,NULL,0,MPI_COMM_WORLD);
+    free(nums);
 }
