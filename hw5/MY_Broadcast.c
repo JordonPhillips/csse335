0bfa9bc26f70c7ffbdb09e9853aa48edb78add19
Jordon Phillips <phillijk@rose-hulman.edu>
2014-04-10 23:54:12 -0400

    Fixed bug with calculating the adjusted rank

--- hw5/MY_Broadcast.c
+++ hw5/MY_Broadcast.c
@@ -11,14 +11,15 @@ void MY_Broadcast(void *buffer, int count, MPI_Datatype datatype,
     int total_procs;
     MPI_Comm_size(comm, &total_procs);
 
-    int adjusted_rank = root - rank;
+    int adjusted_rank = rank - root;
     if (adjusted_rank < 0)
-        adjusted_rank = total_procs - root - adjusted_rank - 1;
+        adjusted_rank += total_procs;
 
     if (rank != root) {
         int sender = adjusted_rank%2 == 0 ? (adjusted_rank/2)-1 : (adjusted_rank-1)/2;
+	printf("%d receiving from %d (adj)\n", adjusted_rank, sender);
         sender = (sender + root)%total_procs;
-        printf("%d recieving from %d", rank, sender);
+        printf("%d recieving from %d\n", rank, sender);
         MPI_Status status;
         MPI_Recv(buffer, count, datatype, sender, MY_BROADCAST_TAG, comm, &status);
     }
@@ -27,11 +28,11 @@ void MY_Broadcast(void *buffer, int count, MPI_Datatype datatype,
     int target2 = 2*(adjusted_rank + 1);
     if (target1 < total_procs) {
         target1 = (root+target1)%total_procs;
-        printf("%d sending to %d", rank, target1);
+        printf("%d sending to %d\n", rank, target1);
         MPI_Send(buffer, count, datatype, target1, MY_BROADCAST_TAG, comm);
 
         if (target2 < total_procs) {
-            printf("%d sending to %d", rank, target2);
+            printf("%d sending to %d\n", rank, target2);
             target2 = (root+target2)%total_procs;
             MPI_Send(buffer, count, datatype, target2, MY_BROADCAST_TAG, comm);
         }
