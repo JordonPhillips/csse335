76d7f159499da4480c249e16e06a0e0e64ec283f
Jordon Phillips <phillijk@rose-hulman.edu>
2014-04-11 03:34:19 -0400

    first stab at reduce

--- hw5/MY_Reduce.c
+++ hw5/MY_Reduce.c
@@ -1,6 +1,62 @@
 #include <mpi.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MY_REDUCE_TAG 51
+
+int adj_to_rank(int adj, int root, int total_procs);
 
 void MY_Reduce(void *sendbuf, void *recvbuf, int count,
     MPI_Datatype datatype, int OP, int root, MPI_Comm comm) {
+    int rank, total_procs;
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &total_procs);
 
+    int adjusted_rank = rank - root;
+    if (adjusted_rank < 0)
+        adjusted_rank += total_procs;
+
+    int adjusted_total = total_procs%2 == 0 ? total_procs : total_procs - 1;
+    MPI_Status status;
+    int send_size, neighbor;
+
+    MPI_Type_size(datatype, &send_size);
+    int *buff = (int*)malloc(send_size*count);
+    memcpy(buff, sendbuf, send_size*count);
+
+    int i = 1, j,k;
+    while (i < total_procs) {
+        i *= 2;
+        if (adjusted_rank % i == 0) {
+            neighbor = adjusted_rank + i;
+            if (neighbor < total_procs) {
+                neighbor = adj_to_rank(neighbor, root, total_procs);
+                MPI_Recv(buff, count, datatype, neighbor, MY_REDUCE_TAG, comm, &status);
+                for (j = 0; j < count; j++) {
+                    k = (int)*((char*)recvbuf+(i*send_size));
+                    if (OP == 1) {
+                        buff[i] += k;
+                    } else if (OP == 2) {
+                        buff[i] = buff[i] < k ? buff[i] : k;
+                    }
+                }
+            }
+        } else {
+            neighbor = adj_to_rank(adjusted_rank - i, root, total_procs);
+            MPI_Send(buff, count, datatype, neighbor, MY_REDUCE_TAG, comm);
+            break;
+        }
+    }
+
+    if (rank == root) {
+        memcpy(recvbuf, buff, send_size*count);
+    }
+    free(buff);
+}
+
+int adj_to_rank(int adj, int root, int total_procs) {
+    int rank = adj + root;
+    if (rank > total_procs)
+        rank -= total_procs;
+    return rank;
 }
--- hw5/MY_Reduce_driver.c
+++ hw5/MY_Reduce_driver.c
@@ -1,9 +1,9 @@
 #include <mpi.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include "MY_MPI.h"
 
-void master(int total_procs);
-void slave(int rank, int total_procs);
+#define NUM_INTS 4
 
 int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
@@ -11,18 +11,29 @@ int main(int argc, char** argv) {
   MPI_Comm_size(MPI_COMM_WORLD,&total_procs);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
-  if (rank == 0) {
-    master(total_procs);
-  } else {
-    slave(rank, total_procs);
+  int *send = (int*)malloc(NUM_INTS*sizeof(int));
+  int *recv = (int*)malloc(NUM_INTS*sizeof(int));
+
+  int i;
+  for (i = 0; i < NUM_INTS; i++) {
+    send[i] = NUM_INTS*rank+i;
+    recv[i] = 0;
   }
+
+  MY_Reduce(send, recv, NUM_INTS, MPI_INT, 1, 0, MPI_COMM_WORLD);
+
+  if (rank == 0) {
+      printf("Final Results: [%d",recv[0]);
+      for (i = 1; i < NUM_INTS; i++) {
+        printf(",%d",recv[i]);
+      }
+      printf("]\n");
+      fflush(stdout);
+  }
+
+  free(send);
+  free(recv);
 
   MPI_Finalize();
   return 0;
-}
-
-void master(int total_procs) {
-}
-
-void slave(int rank, int total_procs) {
 }
