0125ff580c7c8b55222207dfc6ffdd09b02e6d38
Jordon Phillips <phillijk@rose-hulman.edu>
2014-04-11 01:45:32 -0400

    Accidently recieved significatnly less in the non-roots

--- hw5/MY_AllToAllBroadcast.c
+++ hw5/MY_AllToAllBroadcast.c
@@ -8,8 +8,9 @@ void MY_AllToAllBroadcast(void *sendbuf, int sendcount,
     MPI_Datatype sendtype, void *recvbuf, int recvcount,
     MPI_Datatype recvtype, MPI_Comm comm) {
 
-    int rank;
+    int rank, total_procs;
     MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &total_procs);
     MPI_Status status;
 
     int send_size, recv_size;
@@ -23,13 +24,10 @@ void MY_AllToAllBroadcast(void *sendbuf, int sendcount,
         void *data = recvbuf;
         memcpy(data, sendbuf, send_size);
 
-        int total_procs;
-        MPI_Comm_size(comm, &total_procs);
-
         int i;
         for (i = 1; i < total_procs; i++) {
             data = (char*)data + recv_size;
-            MPI_Recv(data,recvcount,recvtype,i,MY_ATOABC_TAG,comm,&status);
+            MPI_Recv(data,sendcount,sendtype,i,MY_ATOABC_TAG,comm,&status);
         }
 
         for (i = 1; i < total_procs; i++) {
@@ -37,7 +35,7 @@ void MY_AllToAllBroadcast(void *sendbuf, int sendcount,
         }
     } else {
         MPI_Send(sendbuf, sendcount, sendtype, 0, MY_ATOABC_TAG, comm);
-        MPI_Recv(recvbuf, recvcount, recvtype, 0, MY_ATOABC_TAG, comm, &status);
+        MPI_Recv(recvbuf, recvcount*total_procs, recvtype, 0, MY_ATOABC_TAG, comm, &status);
     }
 }
 
