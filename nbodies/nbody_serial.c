a330f6245cd21af63b10f2e6200b868bb4e74dfc
Jordon Phillips <phillijk@rose-hulman.edu>
2014-05-15 16:02:12 -0400

    Serial is finished.

--- nbodies/nbody_serial.c
+++ nbodies/nbody_serial.c
@@ -3,16 +3,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 int main(int argc, char **argv) {
-	if (argc != 4) {
-		printf("Invalid number of arguments. Expecting 4, found %d.\n", argc);
+	if (argc != 5) {
+		printf("Invalid number of arguments. Expecting 4, found %d.\n", argc-1);
 		return -1;
 	}
 	double **particles;
 	int n, i;
 	read_particles(argv[1], &particles, &n);
-	nbody_serial(&particles, n);
+	nbody_serial(&particles, n, (int)atof(argv[3]), (int)atof(argv[4]), G);
 	write_particles(argv[2], particles, n);
 
 	for (i = 0; i < n; i++) {
@@ -22,8 +23,48 @@ int main(int argc, char **argv) {
   return 0;
 }
 
-void nbody_serial(double ***particles, int n) {
-	
+void nbody_serial(double ***particles, int n, int timesteps, int timestep_size, double g) {
+	int i,j;
+	double *forces = (double*)malloc(DIMENSIONS*n*sizeof(double));
+	for (i = timestep_size; i < timesteps; i+=timestep_size) {
+		for (j = 0; j < n; j++) {
+			(*particles)[j][X]  += timestep_size * (*particles)[j][DX];
+			(*particles)[j][Y]  += timestep_size * (*particles)[j][DY];
+		}
+		get_forces(forces, *particles, n, g);
+		for (j = 0; j < n; j++) {
+			(*particles)[j][DX] += timestep_size * forces[DIMENSIONS*j] / (*particles)[j][MASS];
+			(*particles)[j][DY] += timestep_size * forces[DIMENSIONS*j+1] / (*particles)[j][MASS];
+		}
+	}
+	free(forces);
+}
+
+void get_forces(double *forces, double **particles, int n, double g) {
+	int i, j;
+	double dx, dy, distance, distance_cubed, force, fx, fy;
+
+	for (i = 0; i < n*DIMENSIONS; i++) {
+		forces[i] = 0.0;
+	}
+
+	for (i = 0; i < n; i++) {
+		for (j = n - 1; j > i; j--) {
+			dx = particles[i][X] - particles[j][X];
+			dy = particles[i][Y] - particles[j][Y];
+			distance = sqrt(pow(dx,2)+pow(dy,2));
+			distance_cubed = pow(distance,3);
+
+			force = g*particles[i][MASS]*particles[j][MASS]/distance_cubed;
+			fx = force * dx;
+			fy = force * dy;
+
+			forces[DIMENSIONS*i]   += fx;
+			forces[DIMENSIONS*i+1] += fy;
+			forces[DIMENSIONS*j]   -= fx;
+			forces[DIMENSIONS*j+1] -= fy;
+		}
+	}
 }
 
 void read_particles(char *filename, double ***particles, int *n) {
@@ -70,12 +111,12 @@ void write_particles(char *filename, double **particles, int n) {
 	fprintf(fp, "%d\n", n);
 
 	int i, j;
-	int doubles_per_particle = DIMENSIONS * 2 + 1;
+	int doubles_per_particle = DIMENSIONS * 2;
 	for (i = 0; i < n; i++) {
 		for (j = 0; j < doubles_per_particle; j++) {
-			fprintf(fp, "%f, ", particles[i][j]);
+			fprintf(fp, "%e, ", particles[i][j]);
 		}
-		fprintf(fp, "%f\n", particles[i][doubles_per_particle - 1]);
+		fprintf(fp, "%e\n", particles[i][doubles_per_particle]);
 	}
 
 	fclose(fp);
--- nbodies/nbody_serial.h
+++ nbodies/nbody_serial.h
@@ -1,6 +1,13 @@
 #define DIMENSIONS 2
+#define X 0
+#define Y 1
+#define DX 2
+#define DY 3
+#define MASS 4
+#define G .0000000000667384
 #define CHARS_PER_DOUBLE 24
 
 void read_particles(char *filename, double ***particles, int *n);
 void write_particles(char *filename, double **particles, int n);
-void nbody_serial(double ***particles, int n);
+void nbody_serial(double ***particles, int n, int timesteps, int timestep_size, double g);
+void get_forces(double *forces, double **particles, int n, double g);
